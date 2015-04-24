032706ecbbeedca80645edffaf4519415cd61a20
ilbert esculpi <ilbert.esculpi@gmail.com>
2015-04-23 22:12:10 -0430

    adjustment for global paths

--- recipe.js
+++ recipe.js
@@ -1,9 +1,11 @@
 
+var RECIPE_PATH = RECIPES_PATH + '/cakephp2-api';
+var TEMPLATES_PATH = RECIPE_PATH + '/templates';
+var API_PATH = OUTPUT_PATH + '/Plugin/Api';
+
 var fs = require('fs-extra');
 var async = require('async');
-var burner = require('../../burner');
-
-var pluginPath = './output/Plugin/Api';
+var burner = require(BASE_PATH + '/burner');
 
 module.exports = {
     
@@ -32,16 +34,16 @@ var init = function(json, callback) {
         
     async.series([
         function(callback) {
-            fs.emptyDir(pluginPath, callback);
+            fs.emptyDir(API_PATH, callback);
         },
         function(callback) {
             burner.mkdirs([
-                pluginPath,
-                pluginPath + '/Config',
-                pluginPath + '/Controller',
-                pluginPath + '/View',
-                pluginPath + '/View/Layouts',
-                pluginPath + '/webroot'
+                API_PATH,
+                API_PATH + '/Config',
+                API_PATH + '/Controller',
+                API_PATH + '/View',
+                API_PATH + '/View/Layouts',
+                API_PATH + '/webroot'
             ], callback);
         }
     ], function(error, results) {
